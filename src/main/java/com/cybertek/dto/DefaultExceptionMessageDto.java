31a78191cf412bb25bf20b984999e126842ba34a
waris0129 <waris0129@hotmail.com>
2021-02-13 13:01:32 -0600

    commit5: add exception

---
+++ src/main/java/com/cybertek/dto/DefaultExceptionMessageDto.java
@@ -0,0 +1,15 @@
+package com.cybertek.dto;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class DefaultExceptionMessageDto {
+
+    private String message;
+
+}
